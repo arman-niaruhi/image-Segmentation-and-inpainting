6f1de9003f2c80256dadb82c0ab989e573837801
armanniaruhi <74722760+arman-niaruhi@users.noreply.github.com>
2023-05-12 21:02:03 +0200

    Update Network.py

--- Segmentation/Network.py
+++ Segmentation/Network.py
@@ -3,22 +3,24 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Net(nn.Module):
-    def __init__(self, input_size, n_hidden1, n_hidden2, output_size):
+    def __init__(self, input_size, n_hidden1, n_hidden2, n_hidden3, output_size):
         super().__init__()
 
         output_size = 1
         self.fc1 = nn.Linear(input_size, n_hidden1) 
         self.fc2 = nn.Linear(n_hidden1, n_hidden2) 
-        self.fc3 = nn.Linear(n_hidden2, output_size) 
+        self.fc3 = nn.Linear(n_hidden2, n_hidden3) 
+        # self.fc4 = nn.Linear(n_hidden3, n_hidden4) 
+        self.fc5 = nn.Linear(n_hidden3, output_size) 
 
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        x = F.leaky_relu(self.fc1(x)) # leakyrelu
+        x = F.leaky_relu(self.fc1(x)) 
         x = F.leaky_relu(self.fc2(x))
-        x = self.fc3(x)
+        x = F.leaky_relu(self.fc3(x))
+        # x = F.leaky_relu(self.fc4(x))
+        x = self.fc5(x)
         #x = self.sigmoid(x)
         return x
 
-
-    
